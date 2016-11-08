c9757c5b70fc0bba81565537554b89d919b0ef04
Fernando Montoya <montogeek@gmail.com>
2016-11-08 08:41:51 -0500

    Updated definition

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,11 +2,13 @@ import gulp from 'gulp'
 import babel from 'gulp-babel'
 import cache from 'gulp-cached'
 
+const flow = 'src/index.js.flow';
+
 const paths = {
   src: 'src/**/*'
 }
 
-gulp.task('transpile', () => gulp.src(paths.src)
+gulp.task('transpile', ['flow'], () => gulp.src([paths.src, '!' + flow])
   .pipe(cache('src'))
   .pipe(babel())
   .pipe(gulp.dest('dist')))
@@ -15,4 +17,7 @@ gulp.task('watch', () => {
   gulp.watch(paths.src, ['transpile'])
 })
 
-gulp.task('default', ['watch', 'transpile'])
+gulp.task('flow', () => gulp.src(flow)
+  .pipe(gulp.dest('dist')))
+
+gulp.task('default', ['watch', 'transpile', 'flow'])
